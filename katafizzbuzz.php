cd79d24360913f40086bfe08f9e022a973a2aa64
Martijn De Letter <martijn@ibuildings.nl>
2013-06-06 14:57:08 +0200

    finished the loop, start building our testsuite

--- katafizzbuzz.php
+++ katafizzbuzz.php
@@ -2,8 +2,12 @@
 header('Content-Type: text/plain');
 
 for ($i=1; $i <= 100 ; $i++) {
-    if ($i % 3 == 0){
-        print 'fizz';
+    if ($i % 3 == 0 && $i % 5 == 0){
+        print 'FizzBuzz';
+    } elseif ($i % 3 == 0){
+        print 'Fizz';
+    } elseif ($i % 5 == 0){
+        print 'Buzz';
     } else {
         print $i;
     }
